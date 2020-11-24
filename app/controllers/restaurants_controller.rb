3102a625f848fc79013e3d0a2c717c40ae8b69c6
Ben Jardine <jardine_199@hotmail.co.uk>
2020-11-24 16:40:47 +0000

    filter results js working properly

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,7 @@ class RestaurantsController < ApplicationController
 
 def index
   @restaurants = Restaurant.all
+  @restaurants = @restaurants.where(category: params[:category]) if params[:category].present?
 end
 
 def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,4 +31,23 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
+
+console.log("doc ready")
+var dropdown = document.getElementsByClassName("dropdown-btn");
+
+for (var i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    console.log("click", i)
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+
+}});
+
+
+
