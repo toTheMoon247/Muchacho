b3cae8cdbdadc961bd7ce3156a58f4bb61e5a22b
Henri Bode <hcbode6@gmail.com>
2020-11-25 17:21:42 +0000

    searchbar fix

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
       @restaurants = Restaurant.all
-      @restaurants = @restaurants.near(params[:query], 2)
+      @restaurants = @restaurants.near(params[:query], 6)
       # @restaurants = @restaurants.global_search(params[:query]) if params[:query]
       @restaurants = @restaurants.where(category: params[:category]) if params[:category]
       # @restaurants = @restaurants.where(dietary: params[:dietary]) if params[:dietary]
