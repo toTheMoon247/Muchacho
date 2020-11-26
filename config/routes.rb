96855ec360b86d54f33d78ad978791b3b4cf11c4
Henri Bode <hcbode6@gmail.com>
2020-11-26 15:00:35 +0000

    new dish

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :dishes, only:[:create, :new]
     resources :orders, only: [:new, :create, :show]
   end
   resources :orders, only: :index
