11099f3ff505a1f5801fd6858b40c91676a9871a
Henri Bode <hcbode6@gmail.com>
2020-11-26 12:20:49 +0000

    admin

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,4 +13,23 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @order = Order.new
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params)
+    if @restaurant.save!
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :new
+    end
+  end
+
+   private
+
+  def strong_params
+    params.require(:restaurant).permit(:name, :address, :description, :dietary, :category, :photo)
+  end
 end
--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -5,3 +5,32 @@ const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
 
+$(function () {
+
+    var maxL = 60;
+
+    $('.content').each(function () {
+
+        var text = $(this).text();
+        if(text.length > maxL) {
+
+            var begin = text.substr(0, maxL),
+                end = text.substr(maxL);
+
+            $(this).html(begin)
+                .append($('<a class="readmore"/>').attr('href', '#').html('  read more...'))
+                .append($('<div class="hidden" />').html(end));
+
+
+        }
+
+
+    });
+
+    $(document).on('click', '.readmore', function () {
+        // $(this).next('.readmore').fadeOut("400");
+        $(this).next('.hidden').slideToggle(400);
+    })
+
+
+})
