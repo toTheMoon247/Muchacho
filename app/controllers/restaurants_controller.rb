9a2836007970da98e0734e83520730938e40e2d1
Ben Jardine <jardine_199@hotmail.co.uk>
2020-11-26 11:13:07 +0000

    filtering now implemented with simpleform

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,26 @@
 class RestaurantsController < ApplicationController
 
-
-def index
-  @restaurants = Restaurant.all
-  @restaurants = @restaurants.where(category: params[:category]) if params[:category].present?
-end
-
+  def index
+    if params[:search].present?
+      if params[:search][:categories].last.present? || params[:search][:dietary].last.present?
+        categories = params[:search][:categories][1..-1]
+        @category_instances =  categories.map do |category|
+         Restaurant.where(category: category)
+        end.flatten
+        dietary = params[:search][:dietary][1..-1]
+        @dietary_instances =  dietary.map do |diet|
+         Restaurant.where(dietary: diet)
+        end.flatten
+        @restaurants = (@category_instances + @dietary_instances).uniq
+      else
+        @restaurants = Restaurant.all
+      end
+    else
+      @restaurants = Restaurant.all
+    end
+  end
 
   def show
     @restaurant = Restaurant.find(params[:id])
   end
-
 end
