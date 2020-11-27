ed1367e67ca04c31066da530eac9e072db71e23e
Sam H <samuel1haim@gmail.com>
2020-11-27 11:21:55 +0000

    In progress

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,21 @@
 class RestaurantsController < ApplicationController
 
-def index
-  @restaurants = Restaurant.all
-end
+  def index
+    @restaurants = Restaurant.all
+  end
 
-def show
-  @restaurant = Restaurant.find(params[:id])
-end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params)
+    redirect_to restaurants_path(@restaurant.id) if @restaurant.save!
+    render :new
+  end
 
 end
